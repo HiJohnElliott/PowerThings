e1b46154067f1fe37d45f6ffad5ee004b54646a4
John Elliott <heyjohnelliott@gmail.com>
2025-04-20 12:15:35 -0400

    Cleaned up the repo so that Things files are in their own directory, gitignore is cleaned up and unneccesary files are removed.

--- ThingSync.py
+++ ThingSync.py
@@ -5,41 +5,7 @@ This program will routinely check your tasks in Things and if they are not synce
 This program will also check on the times of existing task-events in Calendar and if the time of the event differs from the task, the task will be updated. 
 """
 
-# import statements 
-from datetime import datetime, time
-from pprint import pprint
-import time
-import api as things
-
-def get_upcoming_tasks():
-    return things.upcoming()
-
-
-def get_upcoming_calendar_events():
-    ...
-
-def add_calendar_event(event_name: str, event_date: str, event_time: str, details: str, task_url: str):
-    ...
-
-
-def update_things_task(task_id: str):
-    # This will inherit functions from makeThings.py
-    ...
-
-
 
 
 if __name__ == "__main__":
-
-    pprint(get_upcoming_tasks())
-    # for task in get_upcoming_tasks():
-        # if task.get('reminder_time'):
-        # print(f"{task['uuid']:<22} | {task['title']} | {task['start_date']}")
-        
-        
-        
-        # if 'reminder_time' in task:
-        #     print(f"{task['uuid']:<22} | {task['title']} | {task['start_date']}")
-        # if task['area'] == 'Home':
-        #     print(task)
-    # pprint(things.get('7SfVtqmdqSgMzmDS3bTnJR'))
+    ...
