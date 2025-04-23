a22ba9c919382a3710f8d1be36fd2c9f9600ddd2
John Elliott <heyjohnelliott@gmail.com>
2025-04-22 22:44:01 -0400

    Added first main() function and importing of task_controller so that I can start running the app from here.

--- ThingSync.py
+++ ThingSync.py
@@ -4,16 +4,15 @@ This program will routinely check your tasks in Things and if they are not synce
 
 This program will also check on the times of existing task-events in Calendar and if the time of the event differs from the task, the task will be updated. 
 """
-
+import task_controller
 
 def main():
-    ...
+    task_controller.add_new_tasks_to_calendar()
     # check for changes in things tasks
     # if change detected: 
         # Add new tasks to cal 
         # update tasks on cal 
-        
 
 
 if __name__ == "__main__":
-    ...
+    main()
