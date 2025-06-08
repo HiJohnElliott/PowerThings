9576242849583dcc7f1e3ec14ecfdf608c4aec64
HiJohnElliott <heyjohnelliott@gmail.com>
2025-06-08 19:38:25 -0400

    Adding a blank version of the config file for other users to start with.

---
+++ config.py
@@ -0,0 +1,15 @@
+# This is the Google Calendar link that you want to point to to sync your tasks. 
+THINGS_CALENDAR_ID: str = ""
+
+# Completed scope sets how far back you want this application for completed tasks. 
+COMPLETED_SCOPE: str = '7d'
+
+# The default duration is in minutes. Duration tags can be toggled on and off. 
+DEFAULT_DURATION: int = 60
+DURATION_TAGS = True
+
+# Zen Mode removes completed tasks from the calendar. 
+ZEN_MODE: bool = True 
+
+# This is for making any changes to tasks in Things using the applications URL scheme. 
+THINGS_AUTH_TOKEN = ""
