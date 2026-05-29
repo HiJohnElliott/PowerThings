3c4f7323f2a15f0060404e44e67b0539d6f1291b
HiJohnElliott <heyjohnelliott@gmail.com>
2026-05-29 19:54:46 -0400

    Removed run code.

--- Log.py
+++ Log.py
@@ -5,19 +5,3 @@ def Plog(heading: str, **kwargs) -> str:
         body += f"\t{k.replace("_", " ")}: {arg}\n"
     body += f"{'-' * 60}\n"
     return body
-
-
-if __name__ == "__main__":
-    head = "This is a big heading"
-    
-    print(
-        Plog(
-            heading = head,
-            Name_goes_here = "John",
-            Role = "stud",
-            Born = "Nov. 1984",
-            Cock = "8============================================D"
-        )
-    )
-
-    print(len("8============================================D"))
